d8d83348cbc2589c53d96ca56f882ccd34e5f371
xingkyh <1726284922@qq.com>
2020-11-21 22:40:27 +0800

    添加部分牌型的判断方法

--- app/src/main/java/com/example/a25cards/utils/PokerTypeUtils.java
+++ app/src/main/java/com/example/a25cards/utils/PokerTypeUtils.java
@@ -1,6 +1,8 @@
 package com.example.a25cards.utils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author xingkyh
@@ -53,5 +55,103 @@ public class PokerTypeUtils {
             return new PokerType(typeKingBoom, pokers.size(), 15);
         }
         return null;
+    }
+
+    /**
+     * 判断牌型是否为单张，是则返回牌型的对象，否则返回null
+     */
+    public static PokerType isSingle(List<Poker> pokers){
+        if (pokers.size() != 1){
+            return null;
+        }
+        return new PokerType(typeSingle, 1, pokers.get(0).getOrderValue());
+    }
+
+    /**
+     * 判断牌型是否为对子，是则返回牌型的对象，否则返回null
+     */
+    public static PokerType isPair(List<Poker> pokers){
+        if (pokers.size() != 2){
+            return null;
+        }
+        // 是否为同一类型的牌
+        if (pokers.get(0).equals(pokers.get(1))){
+            return new PokerType(typePair, 2, pokers.get(0).getOrderValue());
+        }
+        return null;
+    }
+
+    /**
+     * 判断牌型是否为三张牌，是则返回牌型的对象，否则返回null
+     */
+    public static PokerType isThree(List<Poker> pokers){
+        if (pokers.size() != 3){
+            return null;
+        }
+        // 三张牌是否为同一类型的牌
+        if (pokers.get(0).equals(pokers.get(1)) && pokers.get(1).equals(pokers.get(2))){
+            return new PokerType(typeThree, 3, pokers.get(0).getOrderValue());
+        }
+        return null;
+    }
+
+    /**
+     * 判断牌型是否为三带一对，是则返回牌型的对象，否则返回null
+     */
+    public static PokerType isThreePair(List<Poker> pokers){
+        if (pokers.size() != 5){
+            return null;
+        }
+        Map<Integer, Integer> kind = getKindMap(pokers);// 获取牌的种类以及其对应的数量
+        int value = 0;
+        int pair = 0;// 两张牌的数量
+        int three = 0;// 三张牌的数量
+        for (Map.Entry<Integer, Integer> entry:kind.entrySet()){
+            if (entry.getValue() == 2){
+                pair++;
+            }else if (entry.getValue() == 3){
+                three++;
+                value = entry.getKey();
+            }
+        }
+        if (pair == 1 && three == 1){
+            return new PokerType(typeThreePair, 5, value);
+        }
+        return null;
+    }
+
+    /**
+     * 判断牌型是否为顺子，是则返回牌型的对象，否则返回null
+     */
+    public static PokerType isStraight(List<Poker> pokers){
+        if (pokers.size() < 5 || pokers.size() > 13){
+            return null;
+        }
+        for (int i = 1; i < pokers.size(); i++){
+            // 存在大小王
+            if (pokers.get(i).getOrderValue() >= 14){
+                return null;
+            }
+            // 除第一张牌外，每张牌要比前一张牌小一
+            if (pokers.get(i - 1).getOrderValue() - pokers.get(i).getOrderValue() != 1){
+                return null;
+            }
+        }
+        return new PokerType(typeStraight, pokers.size(), pokers.get(pokers.size() - 1).getOrderValue());
+    }
+
+    /**
+     * 获取牌的种类以及其对应牌数量的map
+      */
+    private static Map<Integer, Integer> getKindMap(List<Poker> pokers){
+        Map<Integer, Integer> kind = new HashMap<>();
+        for (Poker poker:pokers){
+            if (kind.containsKey(poker.getOrderValue())){
+                kind.put(poker.getOrderValue(), kind.get(poker.getOrderValue()) + 1);
+            }else {
+                kind.put(poker.getOrderValue(), 1);
+            }
+        }
+        return kind;
     }
 }
